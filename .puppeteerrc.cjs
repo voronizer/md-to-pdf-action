223dc1054bc760bd3de336508ae9462ea464bf64
Alexander Voronische <voronizer@gmail.com>
2023-03-18 22:44:48 +0200

    fix build package

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -5,5 +5,5 @@ const {join} = require('path');
  */
 module.exports = {
     // Changes the cache location for Puppeteer.
-    cacheDirectory: join('root', '.cache', 'puppeteer'),
+    cacheDirectory: join('/', 'root', '.cache', 'puppeteer'),
 };
